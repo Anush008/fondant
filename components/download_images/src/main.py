a80be1712584ca7002e1114c0b569e7f7be7eb8b
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-10-03 00:19:29 +0200

    Use threads instead of asyncio for downloading

--- components/download_images/src/main.py
+++ components/download_images/src/main.py
@@ -2,36 +2,34 @@
 This component downloads images based on URLs, and resizes them based on various settings like
 minimum image size and aspect ratio.
 """
-import asyncio
 import io
 import logging
 import typing as t
+import urllib.request
+from multiprocessing.pool import ThreadPool
+from threading import Semaphore
 
-import dask
-import httpx
 import pandas as pd
 from fondant.component import PandasTransformComponent
 from resizer import Resizer
 
 logger = logging.getLogger(__name__)
 
-dask.config.set(scheduler='processes')
-
 
 class DownloadImagesComponent(PandasTransformComponent):
     """Component that downloads images based on URLs."""
 
     def __init__(self,
-                 *_,
-                 timeout: int,
-                 retries: int,
-                 n_connections: int,
-                 image_size: int,
-                 resize_mode: str,
-                 resize_only_if_bigger: bool,
-                 min_image_size: int,
-                 max_aspect_ratio: float,
-                 ):
+         *_,
+         timeout: int,
+         retries: int,
+         n_connections: int,
+         image_size: int,
+         resize_mode: str,
+         resize_only_if_bigger: bool,
+         min_image_size: int,
+         max_aspect_ratio: float,
+    ) -> None:
         """Component that downloads images from a list of URLs and executes filtering and resizing.
 
         Args:
@@ -51,7 +49,7 @@ class DownloadImagesComponent(PandasTransformComponent):
         """
         self.timeout = timeout
         self.retries = retries
-        self.n_connections = n_connections
+        self.thread_count = n_connections
         self.resizer = Resizer(
             image_size=image_size,
             resize_mode=resize_mode,
@@ -60,51 +58,61 @@ class DownloadImagesComponent(PandasTransformComponent):
             max_aspect_ratio=max_aspect_ratio,
         )
 
-    async def download_image(self, url: str, *, semaphore: asyncio.Semaphore) -> t.Optional[bytes]:
+    def download_image(self, url: str) -> t.Optional[io.BytesIO]:
         url = url.strip()
 
         user_agent_string = (
             "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
         )
         user_agent_string += " (compatible; +https://github.com/ml6team/fondant)"
+        headers = {"User-Agent": user_agent_string}
 
-        transport = httpx.AsyncHTTPTransport(retries=self.retries)
-        async with httpx.AsyncClient(transport=transport, follow_redirects=True) as client:
-            try:
-                async with semaphore:
-                    response = await client.get(url, timeout=self.timeout,
-                                                headers={"User-Agent": user_agent_string})
-                image_stream = response.content
-            except Exception as e:
-                logger.warning(f"Skipping {url}: {repr(e)}")
-                image_stream = None
-
+        image_stream = None
+        try:
+            request = urllib.request.Request(url, data=None, headers=headers)
+            with urllib.request.urlopen(request, timeout=self.timeout) as response:
+                image_stream = io.BytesIO(response.read())
+            return image_stream
+        except Exception as e:
+            logger.warning(f"Skipping {url}: {repr(e)}")
+            if image_stream is not None:
+                image_stream.close()
+            image_stream = None
         return image_stream
 
-    async def download_and_resize_image(self, id_: str, url: str, *, semaphore: asyncio.Semaphore) \
-            -> t.Tuple[str, t.Optional[bytes], t.Optional[int], t.Optional[int]]:
-        image_stream = await self.download_image(url, semaphore=semaphore)
-        if image_stream is not None:
-            image_stream, width, height = self.resizer(io.BytesIO(image_stream))
-        else:
-            image_stream, width, height = None, None, None
-        return id_, image_stream, width, height
+    def download_image_with_retry(self, id_: str,  url: str) \
+            -> t.Tuple[str, t.Optional[io.BytesIO]]:
+        for _ in range(self.retries + 1):
+            image_stream = self.download_image(url)
+            if image_stream is not None:
+                return id_, image_stream
+        return id_, None
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         logger.info(f"Downloading {len(dataframe)} images...")
 
+        semaphore = Semaphore(self.thread_count * 2)
+
+        def data_generator():
+            for row in dataframe["images"]["url"].items():
+                semaphore.acquire()
+                yield row
+
         results: t.List[t.Tuple[str, bytes, int, int]] = []
+        with ThreadPool(self.thread_count) as thread_pool:
+            for id_, image_stream in thread_pool.imap_unordered(
+                lambda row: self.download_image_with_retry(*row),
+                data_generator(),
+            ):
+                if image_stream is not None:
+                    image, width, height = self.resizer(image_stream)
+                    image_stream.close()
+                    del image_stream
+                else:
+                    image, width, height = None, None, None
 
-        async def download_dataframe() -> None:
-            semaphore = asyncio.Semaphore(self.n_connections)
-
-            images = await asyncio.gather(
-                *[self.download_and_resize_image(id_, url, semaphore=semaphore)
-                  for id_, url in zip(dataframe.index, dataframe["images"]["url"])],
-            )
-            results.extend(images)
-
-        asyncio.run(download_dataframe())
+                semaphore.release()
+                results.append((id_, image, width, height))
 
         columns = ["id", "data", "width", "height"]
         if results:
